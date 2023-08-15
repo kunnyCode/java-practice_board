f951259db19ed09c648d6bddc8a4bf107737b6de
kunny-code <kearnyyoon@gmail.com>
2023-08-15 19:17:27 +0900

    #10 - JPA test 작성
    기본 crud + 연관관계 매핑과 cascading이 잘 동작하는지 보려고 작성
    테스트 데이터와 관련이 있기 때문에, 이대로는 실패
    + yaml파일 미쳐 반영 못한 것도 같이 커밋

---
+++ board-project/src/test/java/com/board/boardproject/repository/JapRepositoryTest.java
@@ -0,0 +1,92 @@
+package com.board.boardproject.repository;
+
+import com.board.boardproject.domain.Article;
+import config.JpaConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JapRepositoryTest {
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    //생성자 주입 패턴
+    public JapRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+
+    @DisplayName("select 테스트")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine() {
+        // Given
+
+        // When
+        List<Article> articles = articleRepository.findAll();
+
+        // Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(1000);
+    }
+
+    @DisplayName("insert 테스트")
+    @Test
+    void givenTestData_whenInserting_thenWorksFine() {
+        //Given
+        Long previousCount = articleRepository.count();
+
+        //When
+        articleRepository.save(Article.of("new article", "new content", "#spring"));
+
+        //Then
+        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+    }
+
+    @DisplayName("update 테스트")
+    @Test
+    void givenTestData_whenUpdating_thenWorksFine() {
+        //Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        String updatedHashtag = "#springboot";
+        article.setHashtag(updatedHashtag);
+
+        //When
+        Article savedArticle = articleRepository.saveAndFlush(article);
+
+        //Then
+        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updatedHashtag);
+    }
+
+    @DisplayName("delete 테스트")
+    @Test
+    void givenTestData_whenDeleting_thenWorksFine() {
+        //Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long previousArticleCount =  articleRepository.count();
+        // 게시글이 삭제되면 연관된 댓글도 사라짐 양방향 바인딩..
+        long previousArticleCommentCount = articleCommentRepository.count();
+        int deletedCommentsSize = article.getArticleComments().size();
+
+        //When
+        articleRepository.delete(article);
+
+        //Then
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
+    }
+}
+
